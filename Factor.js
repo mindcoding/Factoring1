048bc3431863ce53ad91abf097f4dc2e85c92e4f
mindcoding <slimercraft3@gmail.com>
2016-03-31 15:49:25 -0400

    Update Factor.js

--- Factor.js
+++ Factor.js
@@ -5,25 +5,31 @@ var c = 1;
 console.log(a + "x^2 + " + b + "x + " + "c");
 
 //Get Factors
-var t = a * b;
+var total = a * b;
 var gf1;
 var gf2;
 
-var f1 = []
-var f3
-for(; f1 <= t; f1++) {
-   if (t % f1 === 0) {
-      f2 = t/f1;
-      f.push(f1);
-      f3.push(f2);
+for(var f1 = 1; f1 <= total; f1++) {
+   if (total % f1 === 0) {
+      f2 = total % f1;
       if(f1 + f2 == b) {
          if(f1*f2 == t) {
             gf1 = f1;
             gf2 = f2;
+            gcf(a, f1);
+            gcf(f2, c);
          }
       }
    }
 
 }
 
+
+function gcf(h, i) {
+    if ( ! i) {
+        return h;
+    }
+
+    return gcd(i, h % i);
+};
 
