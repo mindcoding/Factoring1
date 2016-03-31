04c07e20c498ae970fe86d0c31b40efd832c993a
mindcoding <slimercraft3@gmail.com>
2016-03-31 17:25:21 -0400

    Finished Factor.js

--- Factor.js
+++ Factor.js
@@ -1,6 +1,6 @@
 var a = 1;
-var b = 5;
-var c = 4;
+var b = 3;
+var c = -18;
 
 console.log(a + "x^2 + " + b + "x + " + "c");
 
@@ -17,17 +17,35 @@ console.log(total)
 for(; f1 <= total; f1++) {
    if (total % f1 === 0) {
       f2 = total / f1;
-	  console.log(f1 + " " + f2)
       if(f1 + f2 == b) {
-		 console.log(f1 + " " + f2)
          if(f1*f2 == total) {
 
             gf1 = f1;
             gf2 = f2;
             gcf1 = gcf(f1, a);
             gcf2 = gcf(f2, c);
-			console.log(gcf1 + " " + gcf2)
 			fac(gcf1, gcf2);
+         }
+      }
+   }
+
+}
+//Negative
+var gf1n;
+var gf2n;
+var gcf1n;
+var gcf2n;
+var f1n = -2;
+for(; f1n >= total; f1n++) {
+   if (total % f1n === 0) {
+      f2n = total / f1n;
+      if(f1n + f2n == b) {
+         if(f1n*f2n == total) {
+            gf1n = f1n;
+            gf2n = f2n;
+            gcf1n = gcf(f1n, a);
+            gcf2n = gcf(f2n, c);
+			facn(gcf1n, gcf2);
          }
       }
    }
@@ -43,11 +61,33 @@ function fac(g, d) {
    var set1 = "(" + s1 + "x" + "+" + s2 + ")(" + gcf1 + "x" + "+" + gcf2 + ")";
    console.log(set1);
 }
+function facn(g, d) {
+   var s1 = a/g;
+   var s2 = gf1n;
+   var s3 = c/d;
+   var s4 = gf2n;
+   var s21 = Math.abs(s2);
+   var gcf2n1 = Math.abs(gcf2n);
+   console.log(s1 + " " + s21 + " " + s3 + " " + s4)
+   if (s2 < 0) {
+	 var set1 = "(" + s1 + "x" + "-" + s21 + ")(" + gcf1n + "x" + "+" + gcf2n1 + ")";
+   }else if (gcf2n < 0) {
+	 var set1 = "(" + s1 + "x" + "+" + s21 + ")(" + gcf1n + "x" + "-" + gcf2n1 + ")";
+   }else if (gcf1n < 0 && s2 < 0) {
+	 var set1 = "(" + s1 + "x" + "-" + s21 + ")(" + gcf1n + "x" + "-" + gcf2n1 + ")";
+   }
+
+   console.log(set1);
+}
 function gcf(h, i) {
+	Math.abs(h);
+	Math.abs(i);
     if ( ! i) {
         return h;
     }
 
+
     return gcf(i, h % i);
+	console.log("H " + i + " " + h)
 	
 };
