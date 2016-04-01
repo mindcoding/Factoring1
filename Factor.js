4a4407124c6665c8db5bdf42757c72fb526b9425
mindcoding <slimercraft3@gmail.com>
2016-04-01 09:21:52 -0400

    Update Factor.js

--- Factor.js
+++ Factor.js
@@ -6,8 +6,8 @@ console.log(a + "x^2 + " + b + "x + " + "c");
 
 //Get Factors
 var total = a * c;
-var gf1;
-var gf2;
+var globalfactor1;
+var globalfactor2;
 var gcf1;
 var gcf2;
 var f1 = 2;
@@ -19,8 +19,8 @@ for(; f1 <= total; f1++) {
 		if(f1 + f2 == b) {
 			if(f1*f2 == total) {
 				console.log("Factors Found")
-				gf1 = f1;
-				gf2 = f2;
+				globalfactor1 = f1;
+				globalfactor2 = f2;
 				gcf1 = gcf(f1, a);
 				gcf2 = gcf(f2, c);
 				fac(gcf1, gcf2);
@@ -53,9 +53,9 @@ for(; f1n >= total; f1n++) {
 function fac(g, d) {
 	console.log("Solved");
 	var s1 = a/g;
-	var s2 = gf1;
+	var s2 = globalfactor1;
 	var s3 = c/d;
-	var s4 = gf2/d;
+	var s4 = globalfactor2/d;
 	 var set1 = "(" + s1 + "x" + "+" + s2 + ")(" + gcf1 + "x" + "+" + gcf2 + ")";
 	console.log(set1);
 }
